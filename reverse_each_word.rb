5fabaa544abc4f1143a35b27b6a08844cf913da7
Alexander Hulteng <alexanderhulteng@gmail.com>
2021-02-03 20:26:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ def reverse_each_word(string)
  phrase = ""
  arr = string.split(" ")
  arr.each do |i|
-   puts i.reverse
+   i.reverse
  end
-
+ arr.join(" ")
 end
 
 reverse_each_word("hey there")
